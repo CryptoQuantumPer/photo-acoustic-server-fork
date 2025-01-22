2ceb081ad3fa9c3152bf52986c2729555bc551be
Nontakorn Bunluesriruang <bunluesriruangnontakorn@gmail.com>
2025-01-22 11:40:00 +0700

    add-files

---
+++ device_interface/sketch_jan16b_copy_20250121232710.ino
@@ -0,0 +1,29 @@
+#include <servo.h>
+
+
+/*
+servo settings
+
+42 for 90+/sec
+120 for 90-/sec
+
+void move90degress(range_mm){
+    servo.move(90) center
+    servo.move
+
+}
+
+*/
+
+void setup(){
+    Serial.begin(115600);
+
+}
+
+void loop(){
+    while (Serial.avaliable()){
+
+    }
+
+
+}
