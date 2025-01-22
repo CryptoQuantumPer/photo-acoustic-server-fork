307636c6fccd29b25e80aaac23fd54f0f6751e3c
Nontakorn Bunluesriruang <bunluesriruangnontakorn@gmail.com>
2025-01-22 13:19:35 +0700

    update sketch_jan16b_

--- device_interface/sketch_jan16b_copy_20250121232710.ino
+++ device_interface/sketch_jan16b_copy_20250121232710.ino
@@ -1,5 +1,5 @@
 #include <servo.h>
-
+int laser_relay_pin = 8;
 
 /*
 servo settings
@@ -12,7 +12,6 @@ void move90degress(range_mm){
     servo.move
 
 }
-
 */
 
 void setup(){
@@ -20,10 +19,42 @@ void setup(){
 
 }
 
+string msg = "";
+const string commands[2] = {"FIRE_LH", "FIRE_LL"};
+
 void loop(){
     while (Serial.avaliable()){
-
+        msg = Serial.readString();
+        if (msg == commands[0]){ activate_laser(HIGH);}
+        if (msg == commands[1]){ activate_laser(LOW); }
+        
+        // run functin if msg is activated
     }
 
+
+}
+
+void activate_laser(int activation){
+    digitalWrite(laser_relay_pin, activation);
+    if (activation == LOW){
+        Serial.println("L_off");
+    }
+    else if (activation == HIGH){
+        Serial.println("L_on");
+    }
+}
+
+// home
+float x = 0;
+float y = 0;
+#define forward_90_per_second 45
+#define backward_90_per_second 125
+
+void move_position(float x, float y){
+    servo.attach(9)
+    servo.attach(8)
+    // translate to seconds
+
+    servo.detach()
 
 }
