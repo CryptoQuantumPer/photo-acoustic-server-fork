d0e29c2599d472882bb833ef558ef13f888afc49
Nontakorn Bunluesriruang <bunluesriruangnontakorn@gmail.com>
2025-02-02 11:28:35 +0700

    save changes

--- device_interface/hantek_acqusition_more_impoved.py
+++ device_interface/hantek_acqusition_more_impoved.py
@@ -833,7 +833,7 @@ class interface(object):
         voltages = operation.convert_read_data(ReadData[channel], scope.VOLT_DIV_INDEX[scope.nCHVoltDIV[channel]][1])
 
         # save voltages npz
-        self.save_npz_extend({"sensor_data_noisy": voltages})
+        operation.save_npz_extend({"sensor_data_noisy": voltages})
 
 
 
